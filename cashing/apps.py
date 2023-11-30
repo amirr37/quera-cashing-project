bac0eb987e75879be8b985579627487169bdb212
Amir Jahangiri <amirrj037@gmail.com>
2023-11-30 22:24:08 +0330

    create app cashing

---
+++ cashing/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CashingConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "cashing"
--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 
     # internal apps
     'account',
+    'cashing',
     # external apps
     'rest_framework',
     'drf_spectacular',
