050fae4b10688bb868db045998d9414dfc4e18eb
Amir Jahangiri <amirrj037@gmail.com>
2023-11-30 22:55:06 +0330

    create transaction and category models

--- cashing/models.py
+++ cashing/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from account.models import CustomUser
+
 
 # Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = [
+        ('income', 'Income'),
+        ('expense', 'Expense'),
+    ]
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.type} - {self.amount} on {self.created_at}"
